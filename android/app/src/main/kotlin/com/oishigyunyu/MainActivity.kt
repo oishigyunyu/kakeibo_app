f1fc76ec89b346419ec940f082d141bc5149abd4
oishigyunyu <yamamuuuura@gmail.com>
2024-01-11 22:48:46 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/oishigyunyu/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.oishigyunyu.kakeibo_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:kakeibo_app/app/app.dart';
+import 'package:kakeibo_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
